8693c9f1776610f67541ffebb21f87d57f13f7a3
raushan52 <raushan52@hotmail.com>
2022-02-13 15:52:18 +0530

    Registration app commit

---
+++ app/src/main/java/com/example/signuppage/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.signuppage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view){
+        Toast.makeText(this,"Logged IN !",Toast.LENGTH_SHORT).show();
+    }
+}
